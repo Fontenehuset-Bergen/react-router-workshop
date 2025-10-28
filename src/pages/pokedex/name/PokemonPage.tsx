8a78d3086523fd668a43efb1bd65c3bebadcf68f
Dragawarrior <Ellenblom22@gmail.com>
2025-10-29 00:28:26 +0100

    updates

--- src/pages/pokedex/name/PokemonPage.tsx
+++ src/pages/pokedex/name/PokemonPage.tsx
@@ -1,4 +1,7 @@
 import type { Pokemon } from "@/types/pokemon";
+import { ArrowLeft, ArrowRight } from "lucide-react";
+import { useEffect, useState } from "react";
+import { useLoaderData } from "react-router";
 
 export async function loader({ params, request }) {
   console.log(`fetching ${params.name}...`);
@@ -19,4 +22,50 @@ export async function loader({ params, request }) {
   return data;
 }
 
-export default function PokemonByName() {}
+export default function PokemonByName() {
+  const pokemon = useLoaderData<typeof loader>();
+  const [images, setImages] = useState<string[]>(["/missigno.png"]);
+  const [index, setIndex] = useState<number>(0);
+
+  useEffect(() => {
+    if (pokemon) {
+      const sprites = Object.values(pokemon.sprites);
+      const spriteUrls = sprites.filter((item) => typeof item == "string");
+
+      if (spriteUrls.length) {
+        setImages(spriteUrls);
+      }
+    }
+  }, [pokemon]);
+  return (
+    <div className="flex-1 flex flex-col gap-4 max-w-96 mx-auto">
+      <div className="relative p-4 bg-stone-900">
+        <button
+          className="absolute left-0 top-1/2 -translate-y-1/2"
+          onClick={() => setIndex(index + 1 < images.length ? index + 1 : 0)}
+        >
+          <ArrowLeft />
+        </button>
+        <img
+          className="size-48 mx-auto object-contain duration-200"
+          src={images.at(index)}
+        />
+        <button
+          className="absolute right-0 top-1/2 -translate-y-1/2"
+          onClick={() => setIndex(index <= 0 ? images.length - 1 : index - 1)}
+        >
+          <ArrowRight />
+        </button>
+      </div>
+      <div>
+        {pokemon?.stats.map((stat) => (
+          <span key={stat.stat.name} className="flex gap-2">
+            <p>{stat.stat.name}</p>
+            <p>{stat.base_stat}</p>
+          </span>
+        ))}
+        {!pokemon && <p className="text-center">A wild missigno appeared!</p>}
+      </div>
+    </div>
+  );
+}
